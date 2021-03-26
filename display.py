a08a47550ebfb7a4f654b1417bc4f40d5751726f
avaparna-max <81403537+avaparna-max@users.noreply.github.com>
2021-03-26 10:52:01 +0530

    Create display.py
    My first file on Github

---
+++ display.py
@@ -0,0 +1,9 @@
+Class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+    
+  def display(self):
+    print("name-{}, age-{}".format(self.name,self.age))
+    return
+  
